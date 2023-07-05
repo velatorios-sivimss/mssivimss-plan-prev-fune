39c3ea8d5fe10cc1a0d6fb3764d524d1a702d455
Eduardo Orlando <eduardo.flores@people-media.com.mx>
2023-07-05 12:58:44 -0600

    num sexo

--- src/main/java/com/imss/sivimss/planfunerario/beans/ModificarConvenioNuevoPf.java
+++ src/main/java/com/imss/sivimss/planfunerario/beans/ModificarConvenioNuevoPf.java
@@ -18,6 +18,29 @@ public class ModificarConvenioNuevoPf {
         queryPersona.agregarParametroValues("NOM_SEGUNDO_APELLIDO", "'" + persona.getSegundoApellido() + "'");
         //queryPersona.agregarParametroValues("NUM_SEXO", "'" + persona.getSexo() + "'");
         queryPersona.agregarParametroValues("DES_OTRO_SEXO", "'" + persona.getOtroSexo() + "'");
+       // queryPersona.agregarParametroValues("FEC_NAC", "'" + persona.getFechaNacimiento() + "'");
+        queryPersona.agregarParametroValues("ID_PAIS", "'" + persona.getPais() + "'");
+        //queryPersona.agregarParametroValues("ID_ESTADO", "'" + persona.getEstado() + "'");
+        queryPersona.agregarParametroValues("DES_TELEFONO", "'" + persona.getTelefono() + "'");
+        queryPersona.agregarParametroValues("DES_CORREO", "'" + persona.getCorreoElectronico() + "'");
+        queryPersona.agregarParametroValues("TIPO_PERSONA", "'" + persona.getTipoPersona() + "'");
+        queryPersona.agregarParametroValues("NUM_INE", "'" + persona.getNumIne() + "'");
+        queryPersona.agregarParametroValues("ID_USUARIO_MODIFICA", usuario);
+        queryPersona.agregarParametroValues("FEC_ACTUALIZACION", "NOW()");
+        queryPersona.addWhere("ID_PERSONA = " + idPersona);
+        return queryPersona.obtenerQueryActualizar();
+    }
+
+    public String generaQueryActualizaPersonaBeneficiario(PersonaAltaConvenio persona, String usuario, String idPersona){
+        final QueryHelper queryPersona = new QueryHelper("UPDATE SVC_PERSONA");
+        queryPersona.agregarParametroValues("CVE_RFC", "'" + persona.getRfc() + "'");
+        queryPersona.agregarParametroValues("CVE_CURP", "'" + persona.getCurp() + "'");
+        queryPersona.agregarParametroValues("CVE_NSS", "'" + persona.getNss() + "'");
+        queryPersona.agregarParametroValues("NOM_PERSONA", "'" + persona.getNombre() + "'");
+        queryPersona.agregarParametroValues("NOM_PRIMER_APELLIDO", "'" + persona.getPrimerApellido() + "'");
+        queryPersona.agregarParametroValues("NOM_SEGUNDO_APELLIDO", "'" + persona.getSegundoApellido() + "'");
+        //queryPersona.agregarParametroValues("NUM_SEXO", "'" + persona.getSexo() + "'");
+        queryPersona.agregarParametroValues("DES_OTRO_SEXO", "'" + persona.getOtroSexo() + "'");
         queryPersona.agregarParametroValues("FEC_NAC", "'" + persona.getFechaNacimiento() + "'");
         queryPersona.agregarParametroValues("ID_PAIS", "'" + persona.getPais() + "'");
         //queryPersona.agregarParametroValues("ID_ESTADO", "'" + persona.getEstado() + "'");
